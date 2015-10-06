1c6067f98a9f0d7345f686f8c9e83b01ecb7322f
Bacher <bacherko@gmail.com>
2015-10-06 12:14:37 +0300

    build refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
 
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var concat = require('gulp-concat');
 var watch = require('gulp-watch');
 var plumber = require('gulp-plumber');
 
-gulp.task('all', function () {
-    return gulp.src('server/**/*.es6')
-        .pipe(babel())
-        .pipe(gulp.dest('server/_compiled'));
-});
-
-gulp.task('default', function () {
+gulp.task('server-es6', function () {
     return gulp.src('server/**/*.es6')
         .pipe(watch('server/**/*.es6'))
         .pipe(plumber())
@@ -18,3 +13,14 @@ gulp.task('default', function () {
         .pipe(plumber.stop())
         .pipe(gulp.dest('server/_compiled'));
 });
+
+gulp.task('client-es6', function () {
+    return gulp.src('www/**/*.es6')
+        .pipe(watch('www/**/*.es6'))
+        .pipe(plumber())
+        .pipe(babel())
+        .pipe(plumber.stop())
+        .pipe(gulp.dest('www/_js'));
+});
+
+gulp.task('default', ['server-es6', 'client-es6']);
